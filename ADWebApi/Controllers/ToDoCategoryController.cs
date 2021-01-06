d8bf36b7165a3c42474c03a286f124e64afa3ad6
Ali Daghayeghi <alidaghayeghi1378@gmail.com>
2021-01-06 12:42:33 +0330

    First commit

---
+++ ADWebApi/Controllers/ToDoCategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADWebApi.Controllers
+{
+    [Route("api/ToDo/{ToDoId}/Categories/{CategoryId}")]
+    [ApiController]
+    public class ToDoCategoryController : ControllerBase
+    {
+        
+        [HttpPost]
+        public IActionResult POST(int ToDoId,int CategoryId)
+        {
+            return Ok();
+        }
+    }
+}
